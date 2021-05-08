ec191edce5c403e5ac1a07d04d0aec27ee6e3eb4
Luca B <a19lucberons@inspedralbes.cat>
2021-05-08 04:47:12 +0200

    Added output HTML/CSS and CSS for fixed position of Output TAB

--- src/components/Multicmd.jsx
+++ src/components/Multicmd.jsx
@@ -17,7 +17,7 @@ function Multicmd() {
                         console.log(message);
                 };*/
                 connect();
-        }, [])
+        });
 
         function connect() {
         var ws = new WebSocket('wss://localhost:5000');
@@ -48,41 +48,41 @@ function Multicmd() {
         }
    return (
         <div class="main">
-            <div class="banner">
-                    <h2>Multi-CMD</h2>
-            </div>
+                <div class="banner">
+                        <h2>Multi-CMD</h2> 
+                </div>
 
-            <div>
-                    <div class="banner" id="endpoints" onClick={()=>{setDispaly("endpoints")}}>
-                            <h2>Select endpoints</h2>
+        <div>
+                <div class="banner" id="endpoints" onClick={()=>{setDispaly("endpoints")}}>
+                        <h2>Select endpoints</h2>
 
-                    </div>
-                    <div class="endpointsDiv" id={display ==="endpoints" ? "openEndpointsDiv" : "" }>
-                            <div class="container">
-                                            <cell class="itemSelect">Select</cell>
-                                            <cell class="itemStatus">Status</cell>
-                                            <cell class="itemEndpointID tableSeparator">Endpoint ID</cell>
-                                            <cell class="itemHostname">Hostname</cell>
-                                            <cell class="tableSeparator"><input type="checkbox"></input></cell>
-                                            <cell>
-                                                    <object className="status-dot" src={redot}></object>
-                                            </cell>
-                                            <cell>e31253-124532</cell>
-                                            <cell class="tableSeparator">Osmar</cell>
-                                            
-                                            <cell class="tableSeparator"><input type="checkbox"></input></cell>
-                                            <cell>
-                                                    <object className="status-dot" src={greendot}></object>
-                                            </cell>
-                                            <cell>e31253-124535</cell>
-                                            <cell class="tableSeparator">Marti</cell>
-                            </div>
-                    </div>
-            </div>
+                </div>
+                <div class="endpointsDiv" id={display ==="endpoints" ? "openEndpointsDiv" : "" }>
+                        <div class="container">
+                                        <cell class="itemSelect">Select</cell>
+                                        <cell class="itemStatus">Status</cell>
+                                        <cell class="itemEndpointID tableSeparator">Endpoint ID</cell>
+                                        <cell class="itemHostname">Hostname</cell>
+                                        <cell class="tableSeparator"><input type="checkbox"></input></cell>
+                                        <cell>
+                                                <object className="status-dot" src={redot}>reddot</object>
+                                        </cell>
+                                        <cell>e31253-124532</cell>
+                                        <cell class="tableSeparator">Osmar</cell>
+                                         
+                                        <cell class="tableSeparator"><input type="checkbox"></input></cell>
+                                        <cell>
+                                                <object className="status-dot" src={greendot}>greendot</object>
+                                        </cell>
+                                        <cell>e31253-124535</cell>
+                                        <cell class="tableSeparator">Marti</cell>
+                                </div>
+                        </div>
+                </div>
 
 
 
-            <div>
+        <div>
                 <div class="banner" id="commands" onClick={()=>{setDispaly("cmd")}}>
                         <h2>Command sender</h2>
                 </div>
@@ -102,11 +102,28 @@ function Multicmd() {
 
             </div>
             <div>
-                <div class="banner" id="output" onClick={()=>{setDispaly("output")}}>
+                <div class={display !== 'output' ? 'fixed banner' : 'banner' } id="output" onClick={()=>{setDispaly("output")}}>
                         <h2>Output result</h2>
                 </div>
                 <div class="outputDiv" id={display ==="output" ? "openOutputDiv" : "" }> 
-                        
+                        <div class="botList">
+                                <div class="botsListed">e31253-124532/Osmar</div>
+                                <div class="botsListed">e31253-124535/Marti</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                                <div class="botsListed">e31253-124534/Other</div>
+                        </div>
+                        <div class="botOutput">
+                                <p>Command: ifconfig</p>
+                                <p>Output: blablalba</p>
+                        </div>
                 </div>
             </div>
         </div>
