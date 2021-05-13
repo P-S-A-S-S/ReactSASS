3d1ecb68dd6db804f6ee85d727e7814e542ad99a
Luca B <a19lucberons@inspedralbes.cat>
2021-05-13 23:52:33 +0200

    Added login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,23 @@
-import '../styles/login.css'
-import logo from '../media/images/sasslogo.png'
-import { useHistory } from 'react-router-dom';
+import '../styles/login.css';
+import logo from '../media/images/sasslogo.png';
+// import { useHistory } from 'react-router-dom';
+import { sendCredentials } from '../helpers/sendCredentials';
+import { useState } from 'react';
 
 function Login() {
-    const history = useHistory();
+    // const history = useHistory();
+    const [ user, setUser ] = useState("");
+    const [ passwd, setPasswd ] = useState("");
+
     return (
         <div className="logincontainer">
             <div className="loginpage">
                 <h1 className="Ltitle">SASS</h1>
                 <h2 className="subtitle">Login</h2>
                 <form className="login">
-                    <input className="inputField" type="text" name="username" placeholder="Username" />
-                    <input className="inputField" type="password" name="password" placeholder="Password" />
-                    <button className="sendform" type="button" name="submit" value="Send" onClick={()=>{history.push("/multi-cmd")}}>Send</button>
+                    <input className="inputField" type="text" name="username" placeholder="Username" onChange={ (e) => { setUser(e.target.value)}} />
+                    <input className="inputField" type="password" name="password" placeholder="Password" onChange={ (e) => { setPasswd(e.target.value)}} />
+                    <button className="sendform" type="button" name="submit" value="Send" onClick={ ()=> {sendCredentials(user, passwd)} }>Send</button>
                 </form>
                 <img className="sasslogo" src={logo} alt="sasslogo"/>
             </div>
