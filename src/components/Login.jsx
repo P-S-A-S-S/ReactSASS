cddc4aff33ce0f856f27c374e440e2a22baa84b0
Luca B <a19lucberons@inspedralbes.cat>
2021-05-14 20:10:31 +0200

    Added login functionality now redirects to homepage, notifications dont work

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,26 @@
 import '../styles/login.css';
 import logo from '../media/images/sasslogo.png';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { sendCredentials } from '../helpers/sendCredentials';
 import { useState } from 'react';
+import 'react-notifications/lib/notifications.css';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 function Login() {
-    // const history = useHistory();
+    const history = useHistory();
     const [ user, setUser ] = useState("");
     const [ passwd, setPasswd ] = useState("");
 
     return (
         <div className="logincontainer">
+            <NotificationContainer/>
             <div className="loginpage">
                 <h1 className="Ltitle">SASS</h1>
                 <h2 className="subtitle">Login</h2>
                 <form className="login">
                     <input className="inputField" type="text" name="username" placeholder="Username" onChange={ (e) => { setUser(e.target.value)}} />
                     <input className="inputField" type="password" name="password" placeholder="Password" onChange={ (e) => { setPasswd(e.target.value)}} />
-                    <button className="sendform" type="button" name="submit" value="Send" onClick={ ()=> {sendCredentials(user, passwd)} }>Send</button>
+                    <button className="sendform" type="button" name="submit" value="Send" onClick={ async ()=> { if(await sendCredentials(user, passwd)){ history.push("/multi-cmd") } else { NotificationManager.success("Command sent") }}}>Send</button>
                 </form>
                 <img className="sasslogo" src={logo} alt="sasslogo"/>
             </div>
