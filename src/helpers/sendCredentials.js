7d32ab62c4bbaf04fa782a9478e823d987599037
Luca B <a19lucberons@inspedralbes.cat>
2021-05-14 20:11:01 +0200

    Now recieves data from backend to manage logging in

--- src/helpers/sendCredentials.js
+++ src/helpers/sendCredentials.js
@@ -1,6 +1,8 @@
-const sendCredentials =  (user, passwd) => {
+var logged = false;
+
+const sendCredentials =  async (user, passwd) => {
     const jsonData = { username: user, password: passwd }
-    fetch('https://localhost:5000/signin', {
+    await fetch('https://localhost:5000/signin', {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
@@ -12,6 +14,11 @@ const sendCredentials =  (user, passwd) => {
         redirect: 'follow', // manual, *follow, error
         referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(jsonData) // body data type must match "Content-Type" header
-      });
-    }
+      })
+        .then(res => res.json()) // expecting a json response
+          .then( (json) => {
+            logged = json.valid;
+          });
+          return logged;
+      }
 export{sendCredentials};
