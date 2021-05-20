241e78d22c0ae2dd870aada699689ad2428a0295
BlackkSight <gtracer.jsx@gmail.com>
2021-05-20 16:28:17 +0200

    history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,26 +1,47 @@
 import '../styles/history.css';
 import Header from './Header';
-function History() {        
-   return (
+import { getHistory } from '../helpers/getHistory';
+import { useEffect, useState } from 'react'
 
-        <div className="main">
-                <Header/>
-                <div>
-                        <div className="banner" id="History">
-                                <h2>History</h2>
-                        </div>
-                        <div className="HistoryDiv" id="openHistoryDiv">
-                                <div className="containerHistory">
-                                        <div className="itemSelectHistory cellHistory">Endpoint</div>
-                                        <div className="itemStatusHistory cellHistory">Command</div>
-                                        <div className="itemEndpointIDHistory cellHistory">Output</div>
-                                        <div className="itemHostnameHistory cellHistory">Date</div>
+function History() {
+        
+        const [ hist, setHist ] = useState([])
+
+        useEffect( async()=>{
+                await setHist(await getHistory())
+                await console.log(hist)
+        }, [])
+        return (
+                <div className="main">
+                        <Header/>
+                        <div>
+                                <div className="banner" id="History">
+                                        <h2>History</h2>
                                 </div>
-                        </div>
+                                <div className="HistoryDiv" id="openHistoryDiv">
+                                        <div className="containerHistory">
+                                                <div className="itemSelectHistory cellHistory">Endpoint</div>
+                                                <div className="itemStatusHistory cellHistory">Command</div>
+                                                <div className="itemEndpointIDHistory cellHistory">Output</div>
+                                                <div className="itemHostnameHistory cellHistory">Date</div>
+                                                { hist.map( row => {
+                                                        return(
+                                                        
+                                                                <>
+                                                                        <div className="hdata">{row.client}</div>
+                                                                        <div className="hdata">{row.cmd}</div>
+                                                                        <div className="otput hdata">{row.output}</div>
+                                                                        <div className="hdata">{row.data}</div>
+                                                                        
+                                                                </>
+                                                        )
+                                                }) }
+                                        </div>
+                                </div>
 
+                        </div>
                 </div>
-        </div>
-    );
-  }
+        );
+        }
   
   export default History;  
